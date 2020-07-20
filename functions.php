75cdb8d021b143c1870fde92cd6236950f1a0737
Steve <anil.stephen66@gmail.com>
2020-07-20 14:03:23 +0530

    Updated header

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /*ENQUEUE STYLES*/
 add_action('wp_enqueue_scripts',function(){
-  wp_enqueue_style('viking-pure-css', get_stylesheet_directory_uri().'/assets/css/viking-pure.css', array('sp-core-style'), '1.0.3' );
+  wp_enqueue_style('viking-pure-css', get_stylesheet_directory_uri().'/assets/css/viking-pure.css', array('sp-core-style'), '1.0.4' );
   wp_enqueue_script( 'video-slider-js', get_stylesheet_directory_uri().'/assets/js/video-slider.js', array( 'jquery' ), '1.0.0', true );
 },99);
 
@@ -15,18 +15,40 @@ add_action('siteorigin_widgets_widget_folders', function( $folders ){
   return $folders;
 });
 
+// Header Right Logos
+function headerRightLogos( $logoType='normal' ){
+  $flag = $logoType == 'white' ? false : true;
+  $img_path = get_stylesheet_directory_uri().'/assets/logos/';
+  $html;
+  if( $flag ){
+    $html .='<img class="logo-one" src="'.$img_path.'greenseal.png"/>';
+  }
+  else{
+    $html .='<img class="logo-one" src="'.$img_path.'greenseal_white.png"/>';
+  }
+  $html .='<img class="logo-two" src="'.$img_path.'woolsafe.png"/>';
+  return $html;
+}
+
 /**
 * Add a custom link to the end of a specific menu that uses the wp_nav_menu() function
 */
 add_filter('wp_nav_menu_items', 'add_admin_link', 10, 2);
 function add_admin_link($items, $args){
-  $img_path = get_stylesheet_directory_uri().'/assets/logos/';
   $html .='<a target="_blank" href="#" class="btn btn-quote">GET A QUOTE</a>';
-  $html .='<img class="logo-one" src="'.$img_path.'greenseal.png"/>';
-  $html .='<img class="logo-two" src="'.$img_path.'woolsafe.png"/>';
+  $html .= headerRightLogos();
 
   if( $args->theme_location == 'primary' ){
-      $items .= '<li class="menu-item navbar-nav pull-right"><div class="header-right">'.$html.'</div></li>';
+      $items .= '<li class="menu-item navbar-nav pull-right header-right-wrapper">
+                  <div class="header-right">
+                    '.$html.'
+                  </div>
+                </li>
+                <li class="viking-logos">
+                  <div class="logos-wrapper">
+                    '.headerRightLogos('white').'
+                  </div>
+                </li>';
   }
   return $items;
 }
--- lib/custom-header/header-template.php
+++ lib/custom-header/header-template.php
@@ -3,11 +3,14 @@
     <div class="container-fluid"><!-- .container-->
       <!-- Brand and toggle get grouped for better mobile display -->
   		<div class="navbar-header">
+        <ul class="list-inline pull-right small-header-logo">
+          <li><?php echo headerRightLogos();?></li>
+        </ul>
   			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" aria-controls="navbar">
   				<span class="sr-only">Toggle navigation</span>
-  				<span class="icon-bar"></span>
-  				<span class="icon-bar"></span>
-  				<span class="icon-bar"></span>
+  				<span class="icon-bar top-bar "></span>
+  				<span class="icon-bar middle-bar"></span>
+  				<span class="icon-bar bottom-bar"></span>
   			</button>
   			<?php do_action('sp_logo');?>
   		</div>
