8858a5ba9391e6393c1e50cac340871588439ad9
Steve <anil.stephen66@gmail.com>
2020-07-27 18:31:25 +0530

    Updated video-popup template

--- partials/video-popup.php
+++ partials/video-popup.php
@@ -4,9 +4,10 @@ $embed_url = str_replace( 'https://www.youtube.com/watch?v=', 'https://www.youtu
 $url_components = parse_url( $video_url );
 $video_id = substr( $url_components['query'], 2 );
 $modal_id =  get_unique_id( $video_url );
+$frame_id = get_unique_id( $video_url.'wrapper' );
 $thumbnail = "http://img.youtube.com/vi/$video_id/mqdefault.jpg";
 ?>
-<div class="viking-video-popup">
+<div id="video-<?php _e( $frame_id );?>" class="viking-video-popup">
   <div class="youtube-frame popup">
     <a class="play-btn" href="#<?php _e( $modal_id );?>" data-toggle="modal">
       <div class="overlay"></div>
@@ -15,5 +16,5 @@ $thumbnail = "http://img.youtube.com/vi/$video_id/mqdefault.jpg";
   <?php the_youtube_modal( $modal_id, $embed_url );?>
 </div>
 <style>
-.youtube-frame.popup{background-image: url(<?php _e( $thumbnail );?>);}
+<?php _e( '#video-'.$frame_id )?> .youtube-frame.popup{background-image: url(<?php _e( $thumbnail );?>);}
 </style>
