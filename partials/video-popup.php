84287a7181aae2b38e7faefabc71df1a838fb807
Steve <anil.stephen66@gmail.com>
2020-07-28 11:49:03 +0530

    Updated video-popup template

--- partials/video-popup.php
+++ partials/video-popup.php
@@ -1,4 +1,5 @@
 <?php
+$img_path = get_stylesheet_directory_uri().'/assets/logos/';
 $video_url = $atts['video_url'];
 $embed_url = get_youtube_link( $video_url  );
 $thumbnail = get_video_thumbnail( $video_url );
@@ -14,5 +15,10 @@ $frame_id = get_unique_id( $video_url.'wrapper' );
   <?php the_youtube_modal( $modal_id, $embed_url );?>
 </div>
 <style>
-<?php _e( '#video-'.$frame_id )?> .youtube-frame.popup{background-image: url(<?php _e( $thumbnail );?>);}
+<?php _e( '#video-'.$frame_id )?> .youtube-frame.popup{
+  background-image: url(<?php _e( $thumbnail );?>);
+}
+.youtube-frame .play-btn:after {
+	content: url('<?php _e( $img_path.'/play-btn.png' );?>');
+}
 </style>
