d142ae48cf9dc9fd6637a8d32dbc8ed56b5c20fd
Steve <anil.stephen66@gmail.com>
2020-07-30 18:51:35 +0530

    Added icons

--- partials/post-case-study.php
+++ partials/post-case-study.php
@@ -1,7 +1,26 @@
-<?php $btn_url = get_post_meta( get_the_ID(), 'learn_more', true );?>
+<?php
+  global $post;
+  $img_path = get_stylesheet_directory_uri().'/assets/logos';
+  $btn_url = get_post_meta( get_the_ID(), 'learn_more', true );
+  $title = get_the_title();
+  $images = array(
+    'healthcare'            => $img_path.'/health.png',
+    'assisted-living'       => $img_path.'/living.png',
+    'golf-courses'          => $img_path.'/golf.png',
+    'food-restaurants'      => $img_path.'/food.png',
+    'schools'               => $img_path.'/school.png',
+    'hospitality'           => $img_path.'/hospitality.png',
+    'commercial-buildings'  => $img_path.'/building.png',
+  );
+?>
 <div class="case-study-wrapper">
   <div class="case-study-meta">
-    <h5 class="title"><?php the_title();?></h5>
+    <?php foreach ( $images as $key => $image ) :?>
+      <?php if( $post->post_name == $key ):?>
+        <img class="study-icon" src="<?php _e( $image );?>" alt="<?php _e( $key );?>">
+      <?php endif;?>
+    <?php endforeach;?>
+    <h5 class="title"><?php _e( $title );?></h5>
     <p class="desc"><?php the_excerpt();?></p>
   </div>
   <a class="btn btn-learn-more" href="<?php _e( $btn_url );?>" target="_blank">LEARN MORE</a>
