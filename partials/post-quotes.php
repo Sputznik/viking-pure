ae156c271f8ef0bc4004adb1fab112f4cb6ea72e
psychome004 <anil.stephen66@gmail.com>
2020-08-17 13:55:24 +0530

    Updated post-quotes template

--- partials/post-quotes.php
+++ partials/post-quotes.php
@@ -1,19 +1,7 @@
-<?php
-  // Make the quote content to beb bold
-  $author_name = get_post_meta( get_the_ID(), 'author_name', true );
-  $author_image = get_post_meta( get_the_ID(), 'author_avatar', true );
-  $author_desc = get_post_meta( get_the_ID(), 'author_details', true );
-?>
-<div class="blue-decoration"></div>
+<?php $quote_image = get_post_meta( get_the_ID(), 'quote_logo', true );?>
 <div class="quote-content">
   <?php the_content();?>
 </div>
-<div class="meta quotes-author-meta">
-  <div class="media-left">
-		<img class="avatar" src="<?php _e( $author_image );?>">
-	</div>
-	<div class="media-body">
-		<div class="author-name"><?php _e( $author_name );?></div>
-    <span class="author-desc"><?php _e( $author_desc );?></span>
-	</div>
+<div class="quote-logo">
+	<img class="avatar" src="<?php _e( $quote_image );?>" style="width:100%;"/>
 </div>
