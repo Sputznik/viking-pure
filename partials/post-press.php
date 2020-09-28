75a9a87351dbf5d4c4abf815ae41d860d2bb8234
psychome004 <anil.stephen66@gmail.com>
2020-09-28 17:38:35 +0530

    Updated orbit-press template styles

--- partials/post-press.php
+++ partials/post-press.php
@@ -3,8 +3,8 @@
   $btn_url = get_post_meta( get_the_ID(), 'btn_url', true );
 ?>
 <div class="press-release-wrapper">
-  <div class="case-study-meta press-release-meta">
+  <div class="press-release-meta">
     <div class="desc"><?php the_excerpt();?></div>
   </div>
-  <a class="btn btn-learn-more" href="<?php _e( $btn_url );?>" target="_blank"><?php _e( $btn_url );?></a>
+  <a class="btn btn-learn-more" href="<?php _e( $btn_url );?>" target="_blank"><?php _e( $btn_text );?></a>
 </div>
