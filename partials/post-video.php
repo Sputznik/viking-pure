00bbbb2384280e8238fbb17677fdf71ae88d7b42
psychome004 <anil.stephen66@gmail.com>
2020-10-06 12:08:51 +0530

    Updated orbit post-video template

--- partials/post-video.php
+++ partials/post-video.php
@@ -1,9 +1,16 @@
 <?php
   global $youtube;
-  $video_url = get_post_meta( get_the_ID(), 'video_url', true );
+  $video_url    = get_post_meta( get_the_ID(), 'video_url', true );
+  $external_url = get_post_meta( get_the_ID(), 'external_url', true );
+  $image_url    = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
 ?>
-
-<?php $youtube->create_video_thumb( $video_url, '200px' );?>
+<?php if( isset( $external_url ) && $external_url ): ?>
+<div class="sp-youtube-frame" style="background-image: url(<?php _e( $image_url )?>); height: 200px">
+  <a class="play-btn" href="<?php _e( $external_url );?>" target="_blank">
+    <div class="overlay"></div>
+  </a>
+</div>
+<?php else: $youtube->create_video_thumb( $video_url, '200px' ); endif;?>
 <div class="video-meta">
   <h5 class="video-title"><?php the_title();?></h5>
   <div class="video-desc"><?php the_content();?></div>
