9673384ad6be77f968b8a1ad2094b5023bfb7486
psychome004 <anil.stephen66@gmail.com>
2020-10-16 14:16:50 +0530

    Updated header-template

--- lib/custom-header/header-template.php
+++ lib/custom-header/header-template.php
@@ -8,17 +8,13 @@
     <div class="container-fluid"><!-- .container-->
       <!-- Brand and toggle get grouped for better mobile display -->
   		<div class="navbar-header">
-
-				<a href="<?php bloginfo('url');?>" class='main-logo'>
-					<img src="<?php echo get_stylesheet_directory_uri();?>/assets/logos/viking-pure-logo.svg" />
-				</a>
-
   			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" aria-controls="navbar">
   				<span class="sr-only">Toggle navigation</span>
   				<span class="icon-bar top-bar "></span>
   				<span class="icon-bar middle-bar"></span>
   				<span class="icon-bar bottom-bar"></span>
   			</button>
+        <?php do_action('sp_logo');?>
 			</div>
   		<?php do_action('sp_nav_menu');?>
 			<?php echo getCertificationLogos();?>
