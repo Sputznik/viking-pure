ec392f40c18d5dc11100f8c7e8801b45e5538ed6
psychome004 <anil.stephen66@gmail.com>
2021-02-04 14:28:21 +0530

    Updated post-common template

--- partials/post-common.php
+++ partials/post-common.php
@@ -1,13 +1,12 @@
-<?php $article_url = get_post_meta( get_the_ID(), 'linkedin_url', true ); ?>
 <?php if( !empty( get_the_post_thumbnail() ) ) : $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0]; ?>
-  <div class="featured-img" style="background-image: url( <?php _e( $image );?> );">
-    <a class="img-link" href="<?php _e( $article_url );?>"></a>
+  <div class="featured-img" style="background-image: url( <?php _e( $image );?> );" role="img" aria-label="<?php _e( get_the_title( $post->ID ) ); ?>">
+    <a class="img-link" href="<?php the_permalink(); ?>"></a>
   </div>
 <?php endif;?>
 <div class="post-desc">
-  <h3><a href="<?php _e( $article_url );?>"><?php the_title();?></a></h3>
+  <h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
   <?php if( empty( get_the_post_thumbnail() ) ) :?>
     <div class="excerpt"><?php the_excerpt();?></div>
   <?php endif;?>
 </div>
-<a class="read-more" href="<?php _e( $article_url );?>">Read More</a>
+<a class="read-more" href="<?php the_permalink(); ?>">Read More</a>
